da9b1d3174005d685d31f41dc4bc9287f649b215
Sapar Jumabekov <58820192+SapaTech@users.noreply.github.com>
2021-11-27 14:07:57 +0300

    Initial Commit

---
+++ DemoApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  DemoApp
+//
+//  Created by user on 27.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+}
